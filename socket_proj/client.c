db780de59a9b36c0c6e8e7440e1d0c43956b005d
sleep-dev <prud3ntiae@gmail.com>
2015-03-22 20:32:29 -0700

    phase 1 function add

--- socket_proj/client.c
+++ socket_proj/client.c
@@ -5,11 +5,16 @@
 #include<sys/socket.h>
 
 int main(int argc, char **argv){
-    int client_socket;
-    client_socket = socket(PF_INET, SOCK_STREAM, 0);
-    if( client_socket == -1 ){
+    int client_fd;
+    struct sockaddr_in serv_addr;
+
+    client_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if( client_fd == -1 ){
         printf("Fail on make socket\n");
         exit(-1);
     }
+}
+
+int phase1(int fd){
 
 }
