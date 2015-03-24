58e65af6a1723b03f3dd79ec518f7f06031bf137
sleep-dev <prud3ntiae@gmail.com>
2015-03-24 03:21:45 -0700

    make option function/struct

--- socket_proj/client.c
+++ socket_proj/client.c
@@ -4,9 +4,14 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 
+struct option{
+    char ip[16];
+    int port;
+    int protocol;
+};
+
 int main(int argc, char **argv){
     int client_fd;
-    struct sockaddr_in serv_addr;
 
     client_fd = socket(PF_INET, SOCK_STREAM, 0);
     if( client_fd == -1 ){
@@ -15,6 +20,5 @@ int main(int argc, char **argv){
     }
 }
 
-int phase1(int fd){
-
+struct option parse_option(char **argv){
 }
