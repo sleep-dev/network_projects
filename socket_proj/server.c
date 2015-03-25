0b751b9f6206d2c3785b1025ebb49bcb25db8c88
sleep-dev <prud3ntiae@gmail.com>
2015-03-25 03:27:32 -0700

    server bind

--- socket_proj/server.c
+++ socket_proj/server.c
@@ -9,10 +9,26 @@
 
 int main(int argc, char **argv){
     int server_fd;
+    int portno;
+
+    struct sockaddr_in server_addr, client_addr;
 
     server_fd = socket(PF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
+    if(server_fd < 0){
         perror("ERROR opening socket");
         exit(-1);
     }
+
+    portno = 12345;
+
+    memset(&server_addr, 0, sizeof(server_addr));   
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(portno);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+        perror("ERROR binding socket");
+        exit(-1);
+    }
+
 }
