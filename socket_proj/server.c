6f436656b4fed7373b00da7a11acc286d66f3812
sleep-dev <prud3ntiae@gmail.com>
2015-03-25 03:21:31 -0700

    server default socket

---
+++ socket_proj/server.c
@@ -0,0 +1,18 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+
+int main(int argc, char **argv){
+    int server_fd;
+
+    server_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0){
+        perror("ERROR opening socket");
+        exit(-1);
+
+}
